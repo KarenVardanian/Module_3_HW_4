b042c0986185a6702fd1d8e97857b6be4f6c6c2a
KarenVardanian <karen.vardanian@nure.ua>
2021-12-30 20:00:53 +0200

    Добавьте файлы проекта.

---
+++ Module_3_HW_4/First.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_3_HW_4
+{
+    public class First
+    {
+        public EventHandler Handler;
+        public Func<int, int, int> SumHandler;
+        public int result;
+        public int Sum(int a, int b)
+        {
+
+            return a + b;
+
+        }
+        public int Method_two(int a, int b)
+        {
+            SumHandler = Sum;
+            SumHandler += Sum;
+            var tmp = 0;
+            var listOfDelegate = SumHandler.GetInvocationList();
+            foreach (var item in listOfDelegate)
+            {
+                var r = item.DynamicInvoke(a, b);
+                tmp += Convert.ToInt32(r);
+            }
+            Console.WriteLine("Result: " + tmp);
+            return tmp;
+        }
+        public void Try(Func<int, int, int> predicate, int a, int b)
+        {
+            predicate = Method_two;
+            try
+            {
+                predicate.Invoke(a, b);
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
---
+++ Module_3_HW_4/Program.cs
@@ -0,0 +1,75 @@
+﻿using Module_3_HW_4.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_3_HW_4
+{
+  
+    
+    public class Program
+    {
+        static void Main()
+        {
+            var f = new First();
+            Func<int, int, int> a;
+            a = f.Method_two;
+            f.Try(a, 1, 1);
+
+            List<Contact> list = new List<Contact>();
+            var Ivan = new Contact
+            {
+                Name = "Ivan",
+                Age = 25,
+                Phone = "380987345123"
+            };
+            var Petr = new Contact
+            {
+                Name = "Petr",
+                Age = 21,
+                Phone = "380661759854"
+            };
+            var Ivan_Vodaphone = new Contact
+            {
+                Name = "Ivan",
+                Age = 25,
+                Phone = "380991759854"
+            };
+            var Kiril = new Contact
+            {
+                Name = "Kiril",
+                Age = 25,
+                Phone = "380991759643"
+            };
+            var Nikita = new Contact
+            {
+                Name = "Nikita",
+                Age = 43,
+                Phone = "380671759814"
+            };
+            var Sergey = new Contact
+            {
+                Name = "Sergey",
+                Age = 19,
+                Phone = "380738329833"
+            };
+            list.Add(Kiril);
+            list.Add(Ivan);
+            list.Add(Sergey);
+            list.Add(Nikita);
+            list.Add(Ivan_Vodaphone);
+            list.Add(Petr);
+
+            var FirstList = list.Where(x => x.Phone.StartsWith("38099"));
+            var SecondList = list.Where(x => x.Phone.Contains("99")).OrderBy(o=>o.Name);
+            var ThirdList = list.FirstOrDefault(x => x.Age < 22 && x.Name != "Sergey");
+            var FourList = list.FirstOrDefault(x => x.Name.Contains("I"));
+            var FifthList =list.Select(x => x.Name);
+            var ListSix = list.Take(4).Where(x => !x.Phone.StartsWith("38099"));
+            var ListSeven = list.Where(x => x.Age>20).OrderByDescending(o=>o.Age);
+            var ListEight = list.Count(x => x.Age > 21);
+        }
+    }
+}
